49d98bed73f5c689c5fac00850f29078c3e2c824
baguchan <shutux500@gmail.com>
2025-12-06 09:52:48 +0900

    1.21.1-1.1.0

--- src/main/java/baguchi/twilight_crossbow/init/ModItems.java
+++ src/main/java/baguchi/twilight_crossbow/init/ModItems.java
@@ -5,6 +5,7 @@ import baguchi.twilight_crossbow.item.IceCrossBowItem;
 import baguchi.twilight_crossbow.item.TripleCrossBowItem;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.component.ChargedProjectiles;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,7 +13,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEM_REGISTRY = DeferredRegister.createItems(TwilightCrossBow.MODID);
 
-    public static final DeferredItem<Item> TRIPLE_CROSSBOW = ITEM_REGISTRY.register("triple_crossbow", () -> new TripleCrossBowItem((new Item.Properties().durability(412).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).stacksTo(1))));
-    public static final DeferredItem<Item> ICE_CROSSBOW = ITEM_REGISTRY.register("ice_crossbow", () -> new IceCrossBowItem((new Item.Properties().durability(412).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).stacksTo(1))));
+    public static final DeferredItem<Item> TRIPLE_CROSSBOW = ITEM_REGISTRY.register("triple_crossbow", () -> new TripleCrossBowItem((new Item.Properties().rarity(Rarity.UNCOMMON).durability(412).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).stacksTo(1))));
+    public static final DeferredItem<Item> ICE_CROSSBOW = ITEM_REGISTRY.register("ice_crossbow", () -> new IceCrossBowItem((new Item.Properties().rarity(Rarity.UNCOMMON).durability(412).component(DataComponents.CHARGED_PROJECTILES, ChargedProjectiles.EMPTY).stacksTo(1))));
 
 }
