0a6ba685b462cb1f307c36e421a326e27e7ef078
baguchan <shutux500@gmail.com>
2025-12-23 19:50:14 +0900

    1.21.1-1.3.2 fix seeker crossbow render

--- src/main/java/baguchi/twilight_crossbow/mixin/ItemInHandRendererMixin.java
+++ src/main/java/baguchi/twilight_crossbow/mixin/ItemInHandRendererMixin.java
@@ -4,7 +4,6 @@ import baguchi.twilight_crossbow.init.ModItems;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,7 +14,7 @@ public class ItemInHandRendererMixin {
 
     @Inject(method = "isChargedCrossbow", at = @At("HEAD"), cancellable = true)
     private static void isChargedCrossbow(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if(stack.is(ModItems.TRIPLE_CROSSBOW) || stack.is(ModItems.ICE_CROSSBOW)) {
+        if (stack.is(ModItems.TRIPLE_CROSSBOW) || stack.is(ModItems.ICE_CROSSBOW) || stack.is(ModItems.SEEKER_CROSSBOW)) {
             cir.setReturnValue(CrossbowItem.isCharged(stack));
         }
     }
